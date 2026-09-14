c1283c997b4ce98ed516c4c8de9acbc0f5c47707
Simon tech <officialsimondon@gmail.com>
2026-09-14 02:15:05 +0100

    Update WhatsApp service with improved Puppeteer configuration for Render deployment

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -19,7 +19,16 @@ class WhatsappService {
         authStrategy: new LocalAuth({ clientId: sessionName }),
         headless: process.env.WHATSAPP_HEADLESS !== 'false',
         puppeteer: {
-          args: ['--no-sandbox', '--disable-setuid-sandbox']
+          headless: 'new',
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--disable-web-security',
+            '--disable-features=IsolateOrigins,site-per-process'
+          ],
+          executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
         }
       });
 
